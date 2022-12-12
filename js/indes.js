c5a1849d8922606f58c7df271d2d6ae087f34106
Akshay Mathur <akshaymathur242@gmail.com>
2022-12-13 00:28:52 +0530

    third

---
+++ js/indes.js
@@ -0,0 +1,4 @@
+alert("Hello"); //Will give alert message when user will enter in the website .
+
+
+alert("Thank you for using the website"); // will give alert message when user will check the location of the particular city.
